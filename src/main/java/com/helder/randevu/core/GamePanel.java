ca3046c029bdb02385ae331520a8dba770d957a1
Hélder Ferreira <helder654@gmail.com>
2026-07-01 23:46:09 -0300

    feat(player): implementa movimentação básica com teclado

--- src/main/java/com/helder/randevu/core/GamePanel.java
+++ src/main/java/com/helder/randevu/core/GamePanel.java
@@ -2,13 +2,21 @@ package com.helder.randevu.core;
 
 import javax.swing.JPanel;
 import java.awt.*;
+import com.helder.randevu.entity.Player;
+import com.helder.randevu.input.KeyHandler;
 
-public class GamePanel extends JPanel implements Runnable{
+public class GamePanel extends JPanel implements Runnable {
     private Thread gameThread;
     private boolean running;
+    private Player player;
+    private KeyHandler keyHandler;
 
     public GamePanel() {
-       gameThread = new Thread(this);
+        gameThread = new Thread(this);
+        keyHandler = new KeyHandler();
+        player = new Player(keyHandler);
+        addKeyListener(keyHandler);
+        setFocusable(true);
     }
 
     @Override
@@ -22,7 +30,7 @@ public class GamePanel extends JPanel implements Runnable{
             long currentTime = System.nanoTime();
             long elapsedTime = currentTime - lastTime;
             delta += elapsedTime / drawInterval;
-            if(delta >= 1) {
+            if (delta >= 1) {
                 update();
                 repaint();
                 delta--;
@@ -31,19 +39,21 @@ public class GamePanel extends JPanel implements Runnable{
 
         }
     }
+
     public void startGameThread() {
         running = true;
         gameThread.start();
     }
-    private void update() {
 
+    private void update() {
+        player.update();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setColor(Color.BLUE);
-        g.fillRect(100, 50, 200, 80);
-
+        Graphics2D g2 = (Graphics2D) g;
+        player.draw(g2);
+        g2.dispose();
     }
 }
---
+++ src/main/java/com/helder/randevu/entity/Player.java
@@ -0,0 +1,46 @@
+package com.helder.randevu.entity;
+
+import java.awt.Color;
+
+import java.awt.Graphics2D;
+
+import com.helder.randevu.input.KeyHandler;
+
+public class Player {
+
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private int speed;
+    private KeyHandler keyHandler;
+    
+    public Player(KeyHandler keyHandler) {
+        this.keyHandler = keyHandler;
+        this.x = 100;
+        this.y = 100;
+        this.width = 32;
+        this.height = 32;
+        this.speed = 4;
+    }
+
+    public void update() {
+        if (keyHandler.upPressed) {
+            y -= speed;
+        }
+        if (keyHandler.downPressed) {
+            y += speed;
+        }
+        if (keyHandler.leftPressed) {
+            x -= speed;
+        }
+        if (keyHandler.rightPressed) {
+            x += speed;
+        }
+    }
+
+    public void draw(Graphics2D g2) {
+    g2.setColor(Color.WHITE);
+    g2.fillRect(x, y, width, height);
+}
+}
