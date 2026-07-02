06fbfd1e1567a5c41491cee6a981ae56dee7c844
Hélder Ferreira <helder654@gmail.com>
2026-07-02 19:08:00 -0300

    feat(player): adiciona direção ao movimento do jogador

--- src/main/java/com/helder/randevu/entity/Player.java
+++ src/main/java/com/helder/randevu/entity/Player.java
@@ -14,6 +14,7 @@ public class Player {
     private int height;
     private int speed;
     private KeyHandler keyHandler;
+    private String direction;
     
     public Player(KeyHandler keyHandler) {
         this.keyHandler = keyHandler;
@@ -22,19 +23,24 @@ public class Player {
         this.width = 32;
         this.height = 32;
         this.speed = 4;
+        direction = "down";
     }
 
     public void update(int screenWidth, int screenHeight) {
         if (keyHandler.upPressed) {
+           direction = "up";
             y -= speed;
         }
         if (keyHandler.downPressed) {
+            direction = "down";
             y += speed;
         }
         if (keyHandler.leftPressed) {
+            direction = "left";
             x -= speed;
         }
         if (keyHandler.rightPressed) {
+            direction = "right";
             x += speed;
         }
         if (x < 0) {
@@ -54,5 +60,6 @@ public class Player {
     public void draw(Graphics2D g2) {
     g2.setColor(Color.WHITE);
     g2.fillRect(x, y, width, height);
+    g2.drawString(direction, x, y - 5);
 }
 }
